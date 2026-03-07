0d063d65dc1e5424c186d150651fb208e534d840
Antonio <antonio-gabriel.lipan@s.unibuc.ro>
2026-03-07 16:49:25 +0200

    Initial project setup

---
+++ src/main/java/com/awbd/vetclinic/VetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.awbd.vetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetClinicApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(VetClinicApplication.class, args);
+    }
+
+}
